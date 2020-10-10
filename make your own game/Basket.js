fadf7cf33d85a3a2d825c35e15ada20f1fb89606
sahana-code <65715572+sahana-code@users.noreply.github.com>
2020-10-10 11:39:07 +0530

    Add files via upload

---
+++ make your own game/Basket.js
@@ -0,0 +1,18 @@
+class Basket {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.image = loadImage("sprites/bird.png");
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("brown");
+     
+    }
+  };
---
+++ make your own game/Bird.js
@@ -0,0 +1,20 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/ball.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+    }
+   
+
+    
+  }
+
